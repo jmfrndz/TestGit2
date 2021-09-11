46c205124bdcfeae43e991ff4e92cd870912447d
JMFF <JMFF@DESKTOP-JE8VNSQ>
2021-09-11 09:35:05 +0200

    Añadido usuario

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -3,5 +3,8 @@ package ejemplo;
 public class Ejemplo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!!");
+		String name = "Peter";
+		String surname = "Martin";
+		System.out.println("Usuario: " + name + surname);
 	}
 }
